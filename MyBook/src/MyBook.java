30d37b6c368c081b0928369d5de5674730dbc757
crocodile3 <yrt333333@163.com>
2018-05-08 23:30:21 +0800

    day01

---
+++ MyBook/src/MyBook.java
@@ -0,0 +1,9 @@
+
+public class MyBook {
+	private static String say = "《Java从入门到精通》";
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+			System.out.println(say+"是我学习Java语言的最佳书籍");
+	}
+
+}
