9588fc850b331535ec9aca3ee994aed323fd3394
crocodile3 <yrt333333@163.com>
2018-05-09 21:44:18 +0800

    day02练习代码

---
+++ item/src/Number/Gess.java
@@ -0,0 +1,14 @@
+package Number;
+
+public class Gess {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		char word = 'd',word2 = '@';
+		int p =23405,p2 = 45213;
+		System.out.println("d在Unicode表中的顺序位置是："+(int)word);
+		System.out.println("@在在Unicode表中的顺序位置是："+(int)word2);
+		System.out.println("Unicode表中的第23045位置是："+(char)p);
+		System.out.println("Unicode表中的第45213位置是："+(char)p2);
+	}
+}
---
+++ item/src/Number/Myint.java
@@ -0,0 +1,15 @@
+package Number;
+
+public class Myint {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		 byte mybyte = 124;
+		 short myshort = 3264;
+		 int myint = 45784612;
+		 long mylong = 4678451;
+		 long result = mybyte + myshort + myint + mylong;
+		 System.out.println("结果为：" + result);
+	}
+
+}
