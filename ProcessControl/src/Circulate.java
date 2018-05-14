b45767e0f1670de9b0fef129e9e611029c3479f4
crocodile3 <yrt333333@163.com>
2018-05-14 21:37:44 +0800

    更新循环语句

---
+++ ProcessControl/src/Circulate.java
@@ -0,0 +1,14 @@
+
+public class Circulate {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		int sum =0;
+		for(int i=2;i<=100;i+=2){
+			sum =sum+i;
+		}
+		System.out.println("2到100之间所有的偶数之和为："+sum);
+
+	}
+
+}
